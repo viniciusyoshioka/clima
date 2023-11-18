96baf8e09eeca07b913c9ae24c250884298e66ab
Vinicius Yoshioka <71671043+viniciusyoshioka@users.noreply.github.com>
2023-11-18 12:59:47 -0300

    Configured path alias

--- babel.config.js
+++ babel.config.js
@@ -6,6 +6,17 @@ module.exports = {
         },
     },
     plugins: [
+        ["module-resolver", {
+            root: ["."],
+            extensions: [".js", ".ts", ".tsx", ".json"],
+            alias: {
+                "@api": "./src/api",
+                "@components": "./src/components",
+                "@router": "./src/router",
+                "@screens": "./src/screens",
+                "@services": "./src/services",
+            },
+        } ],
         "react-native-reanimated/plugin",
     ],
 }
