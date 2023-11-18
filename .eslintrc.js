1914477613cc5cf43efd5c3c3c929862bf6c3555
Vinicius Yoshioka <71671043+viniciusyoshioka@users.noreply.github.com>
2023-11-17 22:41:35 -0300

    Initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,8 @@
+/** @type {import('eslint').Linter.Config} */
+module.exports = {
+    root: true,
+    extends: [
+        "@elementium/eslint-config",
+        "@elementium/eslint-config-typescript",
+    ],
+}
