db36b084690889ddc2cc91a622004e89b3fb0f16
Vinicius Yoshioka <71671043+viniciusyoshioka@users.noreply.github.com>
2023-12-01 15:24:09 -0300

    Add types for current weather storage in mmkv

--- src/services/storage.ts
+++ src/services/storage.ts
@@ -1,6 +1,7 @@
 
 export enum STORAGE_KEYS {
     SEARCH_CITY = "search_city",
+    CURRENT_WEATHER = "current_weather",
 }
 
 
@@ -9,4 +10,26 @@ export interface SearchCity {
     latitude: number
     longitude: number
     timestamp: number
+}
+
+
+export interface CurrentWeatherData {
+    weather: {
+        id: number
+        main: string
+        description: string
+        icon: string
+    }
+
+    currentTemperature: number
+    perceivedTemperature: number
+    temperatureMin: number
+    temperatureMax: number
+
+    humidity: number
+
+    sunriseTimestamp: number
+    sunsetTimestamp: number
+
+    windSpeed: number
 }
