abe1849a9dbe3d63567d24e70da31af822d4c1e2
Vinicius Yoshioka <71671043+viniciusyoshioka@users.noreply.github.com>
2023-12-01 17:22:04 -0300

    Moved search logic from Home screen to LocationInput

--- src/screens/Home/LocationInput.tsx
+++ src/screens/Home/LocationInput.tsx
@@ -1,24 +1,31 @@
 import { Color, Prism } from "@elementium/color"
-import { ForwardedRef, forwardRef, useState } from "react"
-import { Pressable, PressableAndroidRippleConfig, StyleProp, StyleSheet, TextInput, TextInputProps, TextStyle, View, ViewStyle } from "react-native"
+import { useState } from "react"
+import { Alert, Pressable, PressableAndroidRippleConfig, StyleProp, StyleSheet, TextInput, TextStyle, View, ViewStyle } from "react-native"
+import { GeoPosition } from "react-native-geolocation-service"
+import { useMMKVObject } from "react-native-mmkv"
 import { Icon, useTheme } from "react-native-paper"
 import { useSafeAreaInsets } from "react-native-safe-area-context"
 
+import { GeocodingResponse, OPEN_WEATHER_MAP, openWeatherMap } from "@api"
+import { Geolocation } from "@services/geolocation"
+import { Permissions } from "@services/permissions"
+import { STORAGE_KEYS, SearchCity } from "@services/storage"
 
-export interface LocationInputProps extends TextInputProps {
-    searchLocation?: () => void
-    getLocation?: () => void
+
+export interface LocationInputProps {
     wrapperStyle?: StyleProp<ViewStyle>
 }
 
 
-export const LocationInput = forwardRef((props: LocationInputProps, ref: ForwardedRef<TextInput>) => {
+export function LocationInput(props: LocationInputProps) {
 
 
     const safeAreaInsets = useSafeAreaInsets()
     const { colors, fonts } = useTheme()
 
     const [text, setText] = useState("")
+    const hasText = text !== undefined && text.length > 0
+    const [citySearch, setCitySearch] = useMMKVObject<SearchCity>(STORAGE_KEYS.SEARCH_CITY)
 
 
     const flattenWrapperStyle = StyleSheet.flatten(props.wrapperStyle)
@@ -29,12 +36,10 @@ export const LocationInput = forwardRef((props: LocationInputProps, ref: Forward
         ...flattenWrapperStyle,
     }
 
-    const flattenInputStyle = StyleSheet.flatten(props.style)
     const inputStyle: TextStyle = {
         ...styles.input,
         ...fonts.titleMedium,
         color: colors.onSurface,
-        ...flattenInputStyle,
     }
 
 
@@ -44,60 +49,153 @@ export const LocationInput = forwardRef((props: LocationInputProps, ref: Forward
     const ripple: PressableAndroidRippleConfig = { color: buttonRipple, radius: 28 }
 
 
-    const baseValue = props.value ?? text
-    const hasValue = baseValue !== undefined && baseValue.length > 0
-
+    function GetLocationButton() {
+        return (
+            <Pressable style={styles.button} onPress={getLocationByGps} android_ripple={ripple}>
+                <Icon source={"map-marker-outline"} size={24} />
+            </Pressable>
+        )
+    }
 
     function SearchLocationButton() {
         return (
-            <Pressable
-                style={styles.button}
-                onPress={props.searchLocation}
-                android_ripple={ripple}
-            >
+            <Pressable style={styles.button} onPress={getLocationBySearch} android_ripple={ripple}>
                 <Icon source={"send"} size={24} />
             </Pressable>
         )
     }
 
-    function GetLocationButton() {
-        return (
-            <Pressable
-                style={styles.button}
-                onPress={props.getLocation}
-                android_ripple={ripple}
-            >
-                <Icon source={"map-marker-outline"} size={24} />
-            </Pressable>
-        )
+
+    async function getLocationByGps() {
+        const locationCoordinates = await getLocationCoordinates()
+        if (!locationCoordinates) return
+
+        const locationAddress = await getLocationAddressFromCoordinates(locationCoordinates)
+        if (!locationAddress) return
+
+        const { coords, timestamp } = locationCoordinates
+        const { latitude, longitude } = coords
+        const city = locationAddress.name
+
+        setText(city)
+        setCitySearch({ city, latitude, longitude, timestamp })
+    }
+
+    async function getLocationCoordinates(): Promise<GeoPosition | undefined> {
+        const hasLocationPermission = await Permissions.requestFineLocation()
+        if (!hasLocationPermission) {
+            Alert.alert(
+                "Não foi possível obter a sua localização",
+                "O uso do GPS é necessário para o funcionamento do aplicativo e sua permissão não foi concedida."
+            )
+            return
+        }
+
+        try {
+            return await Geolocation.getCurrentPosition()
+        } catch (error) {
+            Alert.alert(
+                "Não foi possível obter a sua localização",
+                "Houve um erro ao obter a localização atual do GPS."
+            )
+        }
+        return
+    }
+
+    async function getLocationAddressFromCoordinates(coordinates: GeoPosition): Promise<GeocodingResponse | undefined> {
+        const params = {
+            lat: coordinates.coords.latitude,
+            lon: coordinates.coords.longitude,
+        }
+
+        try {
+            const response = await openWeatherMap.get(OPEN_WEATHER_MAP.routes.reverseGeocoding, { params })
+            const data = response.data as GeocodingResponse[]
+            if (data.length === 0) {
+                Alert.alert(
+                    "Não foi possível obter a sua localização",
+                    "Não foi possível obter a sua cidade com base na localização do GPS"
+                )
+                return
+            }
+
+            return data[0]
+        } catch (error) {
+            Alert.alert(
+                "Não foi possível obter a sua localização",
+                "Houve um erro ao obter a sua cidade com base na localização do GPS."
+            )
+            return
+        }
+    }
+
+
+    async function getLocationBySearch() {
+        if (text === undefined || text.length === 0) {
+            Alert.alert(
+                "Não foi possível pesquisar localização",
+                "O nome da cidade não pode ser vazio."
+            )
+            return
+        }
+
+        const locationCoordinates = await getLocationCoordinatesFromAddress(text)
+        if (!locationCoordinates) return
+
+        const city = locationCoordinates.name
+        const latitude = locationCoordinates.lat
+        const longitude = locationCoordinates.lon
+        const timestamp = Date.now()
+
+        setCitySearch({ city, latitude, longitude, timestamp })
+    }
+
+    async function getLocationCoordinatesFromAddress(cityName: string): Promise<GeocodingResponse | undefined> {
+        const params = { q: cityName }
+
+        try {
+            const response = await openWeatherMap.get(OPEN_WEATHER_MAP.routes.geocoding, { params })
+            const data = response.data as GeocodingResponse[]
+            if (data.length === 0) {
+                Alert.alert(
+                    "Não foi possível obter a sua localização",
+                    "Não foi possível obter suas coordenadas com base na cidade fornecida."
+                )
+                return
+            }
+
+            return data[0]
+        } catch (error) {
+            Alert.alert(
+                "Não foi possível obter a sua localização",
+                "Houve um erro ao obter suas coordenadas com base no nome da cidade fornecida."
+            )
+            return
+        }
     }
 
 
     return (
         <View style={wrapperStyle}>
             <TextInput
+                defaultValue={citySearch?.city}
+                value={text}
+                onChangeText={setText}
                 selectTextOnFocus={true}
                 placeholder={"Pesquisar cidade"}
                 selectionColor={colors.primaryContainer}
                 cursorColor={colors.primary}
                 placeholderTextColor={colors.onSurfaceVariant}
-                {...props}
-                ref={ref}
-                value={props.value ?? text}
-                onChangeText={props.onChangeText ?? setText}
+                onSubmitEditing={getLocationBySearch}
                 style={inputStyle}
             />
 
-            {hasValue && (
-                <SearchLocationButton />
-            )}
+            {!hasText && <GetLocationButton />}
 
-            {!hasValue && (
-                <GetLocationButton />
-            )}
+            {hasText && <SearchLocationButton />}
         </View>
     )
-})
+}
 
 
 const styles = StyleSheet.create({
--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,11 +1,8 @@
-import { useEffect, useState } from "react"
+import { useEffect } from "react"
 import { Alert, View } from "react-native"
-import { GeoPosition } from "react-native-geolocation-service"
 import { useMMKV, useMMKVObject } from "react-native-mmkv"
 
-import { CurrentWeatherResponse, GeocodingResponse, OPEN_WEATHER_MAP, openWeatherMap } from "@api"
-import { Geolocation } from "@services/geolocation"
-import { Permissions } from "@services/permissions"
+import { CurrentWeatherResponse, OPEN_WEATHER_MAP, openWeatherMap } from "@api"
 import { CurrentWeatherData, STORAGE_KEYS, SearchCity } from "@services/storage"
 import { LocationInput } from "./LocationInput"
 import { styles } from "./styles"
@@ -15,118 +12,7 @@ export function Home() {
 
 
     const mmkv = useMMKV()
-    const [citySearch, setCitySearch] = useMMKVObject<SearchCity>(STORAGE_KEYS.SEARCH_CITY)
     const [currentWeather, setCurrentWeather] = useMMKVObject<CurrentWeatherData>(STORAGE_KEYS.CURRENT_WEATHER)
-    const [cityName, setCityName] = useState(citySearch?.city)
-
-
-    async function getLocationByGps() {
-        const locationCoordinates = await getLocationCoordinates()
-        if (!locationCoordinates) return
-
-        const locationAddress = await getLocationAddressFromCoordinates(locationCoordinates)
-        if (!locationAddress) return
-
-        const { coords, timestamp } = locationCoordinates
-        const { latitude, longitude } = coords
-        const city = locationAddress.name
-
-        setCityName(city)
-        setCitySearch({ city, latitude, longitude, timestamp })
-    }
-
-    async function getLocationCoordinates(): Promise<GeoPosition | undefined> {
-        const hasLocationPermission = await Permissions.requestFineLocation()
-        if (!hasLocationPermission) {
-            Alert.alert(
-                "Não foi possível obter a sua localização",
-                "O uso do GPS é necessário para o funcionamento do aplicativo e sua permissão não foi concedida."
-            )
-            return
-        }
-
-        try {
-            return await Geolocation.getCurrentPosition()
-        } catch (error) {
-            Alert.alert(
-                "Não foi possível obter a sua localização",
-                "Houve um erro ao obter a localização atual do GPS."
-            )
-        }
-        return
-    }
-
-    async function getLocationAddressFromCoordinates(coordinates: GeoPosition): Promise<GeocodingResponse | undefined> {
-        const params = {
-            lat: coordinates.coords.latitude,
-            lon: coordinates.coords.longitude,
-        }
-
-        try {
-            const response = await openWeatherMap.get(OPEN_WEATHER_MAP.routes.reverseGeocoding, { params })
-            const data = response.data as GeocodingResponse[]
-            if (data.length === 0) {
-                Alert.alert(
-                    "Não foi possível obter a sua localização",
-                    "Não foi possível obter a sua cidade com base na localização do GPS"
-                )
-                return
-            }
-
-            return data[0]
-        } catch (error) {
-            Alert.alert(
-                "Não foi possível obter a sua localização",
-                "Houve um erro ao obter a sua cidade com base na localização do GPS."
-            )
-            return
-        }
-    }
-
-
-    async function getLocationBySearch() {
-        if (cityName === undefined || cityName.length === 0) {
-            Alert.alert(
-                "Não foi possível pesquisar localização",
-                "O nome da cidade não pode ser vazio."
-            )
-            return
-        }
-
-        const locationCoordinates = await getLocationCoordinatesFromAddress(cityName)
-        if (!locationCoordinates) return
-
-        const city = locationCoordinates.name
-        const latitude = locationCoordinates.lat
-        const longitude = locationCoordinates.lon
-        const timestamp = Date.now()
-
-        setCitySearch({ city, latitude, longitude, timestamp })
-    }
-
-    async function getLocationCoordinatesFromAddress(cityName: string): Promise<GeocodingResponse | undefined> {
-        const params = { q: cityName }
-
-        try {
-            const response = await openWeatherMap.get(OPEN_WEATHER_MAP.routes.geocoding, { params })
-            const data = response.data as GeocodingResponse[]
-            if (data.length === 0) {
-                Alert.alert(
-                    "Não foi possível obter a sua localização",
-                    "Não foi possível obter suas coordenadas com base na cidade fornecida."
-                )
-                return
-            }
-
-            return data[0]
-        } catch (error) {
-            Alert.alert(
-                "Não foi possível obter a sua localização",
-                "Houve um erro ao obter suas coordenadas com base no nome da cidade fornecida."
-            )
-            return
-        }
-    }
 
 
     async function getWeatherData(search: SearchCity) {
@@ -178,14 +64,7 @@ export function Home() {
 
     return (
         <View style={styles.container}>
-            <LocationInput
-                defaultValue={citySearch?.city}
-                value={cityName}
-                onChangeText={setCityName}
-                onSubmitEditing={getLocationBySearch}
-                getLocation={getLocationByGps}
-                searchLocation={getLocationBySearch}
-            />
+            <LocationInput />
         </View>
     )
 }
