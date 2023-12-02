82ef27ca0559c24779c782bc96d613dbc5205304
Vinicius Yoshioka <71671043+viniciusyoshioka@users.noreply.github.com>
2023-12-02 00:11:52 -0300

    Created WeatherData component to show the data when available

---
+++ src/screens/Home/WeatherData.tsx
@@ -0,0 +1,43 @@
+import { ScrollView, StatusBar, StyleSheet, ViewStyle } from "react-native"
+import { useMMKVObject } from "react-native-mmkv"
+
+import { CurrentTemperature } from "@components"
+import { STORAGE_KEYS, SearchCity } from "@services/storage"
+
+
+export function WeatherData() {
+
+
+    const [citySearch, setCitySearch] = useMMKVObject<SearchCity>(STORAGE_KEYS.SEARCH_CITY)
+
+    const contentContainerStyle: ViewStyle = {
+        ...styles.contentContainer,
+        paddingTop: 56 + (2 * 16),
+    }
+
+
+    if (!citySearch) {
+        return null
+    }
+
+
+    return (
+        <ScrollView
+            style={styles.container}
+            contentContainerStyle={contentContainerStyle}
+        >
+            <CurrentTemperature />
+        </ScrollView>
+    )
+}
+
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        marginTop: StatusBar.currentHeight ?? 0,
+    },
+    contentContainer: {
+        padding: 16,
+    },
+})
