2641f2d9646efd4dd20201408a3b5e81cd5205f2
Vinicius Yoshioka <71671043+viniciusyoshioka@users.noreply.github.com>
2024-03-21 12:13:12 -0300

    refactor: Format api

--- src/api/types.ts
+++ src/api/types.ts
@@ -88,54 +88,54 @@ export interface CurrentWeatherResponse {
 
 
 export interface ForecastResponse {
-    "cod": string
-    "message": number
-    "cnt": number
-    "list": Array<{
-        "dt": number
-        "main": {
-            "temp": number
-            "feels_like": number
-            "temp_min": number
-            "temp_max": number
-            "pressure": number
-            "sea_level": number
-            "grnd_level": number
-            "humidity": number
-            "temp_kf": number
+    cod: string
+    message: number
+    cnt: number
+    list: Array<{
+        dt: number
+        main: {
+            temp: number
+            feels_like: number
+            temp_min: number
+            temp_max: number
+            pressure: number
+            sea_level: number
+            grnd_level: number
+            humidity: number
+            temp_kf: number
         }
-        "weather": Array<{
-            "id": number
-            "main": string
-            "description": string
-            "icon": string
+        weather: Array<{
+            id: number
+            main: string
+            description: string
+            icon: string
         }>
-        "clouds": {
-            "all": number
+        clouds: {
+            all: number
         }
-        "wind": {
-            "speed": number
-            "deg": number
-            "gust": number
+        wind: {
+            speed: number
+            deg: number
+            gust: number
         }
-        "visibility": number
-        "pop": number
-        "sys": {
-            "pod": string
+        visibility: number
+        pop: number
+        sys: {
+            pod: string
         }
-        "dt_txt": string
+        dt_txt: string
     }>
-    "city": {
-        "id": number
-        "name": string
-        "coord": {
-            "lat": number
-            "lon": number
+    city: {
+        id: number
+        name: string
+        coord: {
+            lat: number
+            lon: number
         }
-        "country": string
-        "population": number
-        "timezone": number
-        "sunrise": number
-        "sunset": number
+        country: string
+        population: number
+        timezone: number
+        sunrise: number
+        sunset: number
     }
 }
